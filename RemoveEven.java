a65e8b9dfadc8bb349b7a33391a727f7b3fe6b30
Sarthk-Singh <sarthaksinghddn@gmail.com>
2025-05-25 13:14:48 +0530

    added

---
+++ RemoveEven.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class RemoveEven 
+{
+    public void removeEvenLength(ArrayList<String>list) 
+    {
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            String str = it.next();
+            if (str.length() % 2 == 0) {
+                it.remove();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
+        System.out.print("Enter number of Elements: ");
+        int n=sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<n;i++){
+            System.out.print("Enter element: ");
+            String str=sc.nextLine();
+            list.add(str);
+        }
+        RemoveEven obj = new RemoveEven();
+        obj.removeEvenLength(list);
+
+        System.out.println("Filtered list: " + list);
+    }
+}
+
